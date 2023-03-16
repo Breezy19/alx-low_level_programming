3aa777611c2c6688d2ccdf6f451242f26c6bcdea
Akram Freij <freij.akram@gmail.com>
2023-03-16 14:49:49 -0700

    hello world my first code in c cisfun

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function and not using puts function this time
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
