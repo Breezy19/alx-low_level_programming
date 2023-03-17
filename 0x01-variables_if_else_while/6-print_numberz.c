6465913b743827c8f896a1efc561820abcd57c37
Akram Freij <freij.akram@gmail.com>
2023-03-17 15:34:08 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - 6-print_numberz
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
