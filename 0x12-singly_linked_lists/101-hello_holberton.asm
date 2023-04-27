7a155a43cf24688c901393b7abb3de88ed0b14cb
Akram Freij <freij.akram@gmail.com>
2023-04-27 05:25:36 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,9 @@
-section .data
-    message db 'Hello, Holberton', 0
-    format db '%s\n', 0
-
-section .text
-    global main
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    ; prepare arguments for printf
-    mov rdi, format
-    mov rsi, message
-
-    ; call printf
-    xor eax, eax
-    call printf
-
-    ; clean up and return
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov   eax, 0
+          ret
+format: db `Hello, Holberton\n`,0
